09a238f4f1f9036dc854b66c4fe34c763d0bd07c
Conor Tompkins <conorotompkins@gmail.com>
2018-02-20 20:55:59 -0500

    fixed regular expressions, added graphs

--- scripts/mayor_tweets.R
+++ scripts/mayor_tweets.R
@@ -34,27 +34,38 @@ tmls %>%
   )
 
 df_bill <- get_timelines("billpeduto", n = 3200)
-df_bill
+#save_as_csv(df_bill, "data/bill_peduto_tweets.csv")
 
 df_bill %>% 
   filter(str_detect(text, "snow|ice|pothole")) %>% 
-  mutate(date = ymd(str_sub(created_at, 1, 10))) %>% 
-  count(date) %>% 
-  ggplot(aes(date, n)) +
-  geom_point() +
-  geom_smooth()
+  select(status_id, text)
+
+df_bill %>% 
+  filter(str_detect(text, "snow|ice|pothole")) %>% 
+  mutate(date = ymd(str_sub(created_at, 1, 10)),
+         snow = str_detect(text, " snow "),
+         ice = str_detect(text, " ice "),
+         sleet = str_detect(text, " sleet "),
+         rain = str_detect(text, " rain "),
+         pothole = str_detect(text, " pothole ")) %>% 
+  select(status_id, date, text, snow, ice, pothole) %>% 
+  gather(key = topic, value = status, -c(status_id, date, text)) %>% 
+  filter(status == TRUE) -> df_tweets_text
+
+df_tweets_text %>% 
+  count(date, topic) -> df_tweets
 
 
+df_tweets %>% 
+  ggplot(aes(date, n, color = topic)) +
+  geom_point()
 
-
-
-
-
-
-
-
-
-
+df_tweets %>% 
+  ggplot(aes(date, topic, fill = n)) +
+  geom_tile() +
+  coord_equal() +
+  scale_x_date(expand = c(0,0)) +
+  theme(panel.grid = element_blank())
 
 ###Citations
 #https://github.com/mkearney/rtweet
