807b440a95e20c7452051c584b7519ad72f5fdd8
Conor Tompkins <conorotompkins@gmail.com>
2018-02-27 19:21:40 -0500

    added code for word correlation munging and visualization

--- scripts/tidytext_functions.R
+++ scripts/tidytext_functions.R
@@ -52,3 +52,45 @@ visualize_bigrams <- function(bigrams, minimum, title = NULL, subtitle = NULL, c
          caption = caption) +
     theme_void(base_size = 18)
 }
+
+word_correlations <- function(dataframe, custom_stopwords){
+  replace_reg <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
+  unnest_reg <- "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
+  dataframe %>% 
+  filter(is_quote == FALSE, is_retweet == FALSE) %>% 
+  select(status_id, text) %>% 
+  mutate(section = row_number() %/% 10) %>%
+  filter(section > 0) %>%
+  mutate(text = str_replace_all(text, replace_reg, "")) %>%
+  unnest_tokens(word, text, token = "regex", pattern = unnest_reg) %>%
+  filter(!word %in% stop_words$word,
+         !word %in% custom_stopwords,
+         str_detect(word, "[a-z]")) %>% 
+  mutate(word = str_replace(word, "'", ""),
+         word = str_replace(word, "’", ""),
+         word = wordStem(word)) %>% 
+  group_by(word) %>%
+  filter(n() >= 20) %>%
+  pairwise_cor(word, section, sort = TRUE)
+  }
+#replace_reg <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
+#unnest_reg <- "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
+#tidy_tweets <- tweets %>% 
+#  filter(!str_detect(text, "^RT")) %>%
+#  mutate(text = str_replace_all(text, replace_reg, "")) %>%
+#  unnest_tokens(word, text, token = "regex", pattern = unnest_reg) %>%
+#  filter(!word %in% stop_words$word,
+#         str_detect(word, "[a-z]"))
+
+
+visualize_word_correlations <- function(dataframe, title, subtitle, caption){
+  dataframe %>% 
+  filter(correlation > .05) %>%
+  graph_from_data_frame() %>%
+  ggraph(layout = "fr") +
+  geom_edge_link(aes(edge_alpha = correlation, edge_width = correlation), show.legend = FALSE) +
+  geom_node_point(color = "lightblue", size = 5) +
+  geom_node_label(aes(label = name), size = 5, repel = FALSE) +
+  scale_edge_alpha_continuous(range = c(.1, .5)) +
+  theme_void()
+}
