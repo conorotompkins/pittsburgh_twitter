168ad2d8dc43cdca42f18ced209be5dc0405385d
Conor Tompkins <conorotompkins@gmail.com>
2018-03-05 20:35:39 -0500

    added text_size argument, changed max edge width

--- scripts/tidytext_functions.R
+++ scripts/tidytext_functions.R
@@ -33,7 +33,7 @@ count_bigrams_basic <- function(dataset, custom_stopwords) {
     count(word1, word2, sort = TRUE)
 }
 
-visualize_bigrams <- function(bigrams, minimum, title = NULL, subtitle = NULL, caption = NULL) {
+visualize_bigrams <- function(bigrams, minimum, text_size = 3, title = NULL, subtitle = NULL, caption = NULL) {
   set.seed(2016)
   a <- grid::arrow(type = "closed", 
                    length = unit(.1, "inches"))
@@ -43,9 +43,9 @@ visualize_bigrams <- function(bigrams, minimum, title = NULL, subtitle = NULL, c
     graph_from_data_frame() %>%
     ggraph(layout = "fr") +
     geom_node_point(color = "lightblue", size = 3) +
-    geom_node_text(aes(label = name), size = 1, vjust = 1, hjust = 1) +
+    geom_node_text(aes(label = name), size = text_size, vjust = 1, hjust = 1) +
     geom_edge_link(aes(edge_alpha = n, edge_width = n), show.legend = TRUE, arrow = a, end_cap = circle(.25, 'inches')) +
-    scale_edge_width_continuous("Count", range = c(.5, 2)) +
+    scale_edge_width_continuous("Count", range = c(.5, 1.5)) +
     scale_edge_alpha_continuous("Count", range = c(.3, .7)) +
     labs(title = title,
          subtitle = subtitle,
