9c1649e42d362a722e48f7ffd80c18bf5dd8fc0b
Conor Tompkins <conorotompkins@gmail.com>
2018-03-09 09:43:37 -0500

    changed labels for senator twitter usernames, added captions and titles

--- scripts/PA_senators_comparisons.R
+++ scripts/PA_senators_comparisons.R
@@ -5,18 +5,22 @@ library(rtweet)
 library(scales)
 
 theme_set(theme_bw(base_size = 18))
+title <- "@SenBobCasey and @SenToomey tweets"
+caption <- "@conor_tompkins"
 
 #tweets_casey <- get_timelines("SenBobCasey", n = 3200) %>% 
 #  mutate(senator = "Casey")
 
 #save_as_csv(tweets_casey, "data/tweets_casey.csv")
-tweets_casey <- read_csv("data/tweets_casey.tweets.csv")
+tweets_casey <- read_csv("data/tweets_casey.tweets.csv") %>% 
+  mutate(senator = "SenBobCasey")
 
 #tweets_toomey <- get_timelines("SenToomey", n = 3200) %>% 
 #  mutate(senator = "Toomey")
 
 #save_as_csv(tweets_toomey, "data/tweets_toomey.csv")
-tweets_toomey <- read_csv("data/tweets_toomey.tweets.csv")
+tweets_toomey <- read_csv("data/tweets_toomey.tweets.csv") %>% 
+  mutate(senator = "SenToomey")
 
 tweets <- bind_rows(tweets_casey, tweets_toomey)
 
@@ -30,13 +34,13 @@ tweets %>%
   select(senator, status_id, text, is_quote, is_retweet) %>% 
   filter(is_quote == FALSE, is_retweet == FALSE) %>% 
   mutate(text = str_replace_all(text, replace_reg, ""),
-         senator = factor(senator, levels = c("Toomey", "Casey"))) %>% 
+         senator = factor(senator, levels = c("SenToomey", "SenBobCasey"))) %>% 
   count(senator)
 
 tidy_tweets <- tweets %>% 
   select(senator, status_id, text, is_quote, is_retweet) %>% 
   mutate(text = str_replace_all(text, replace_reg, ""),
-         senator = factor(senator, levels = c("Casey", "Toomey"))) %>%
+         senator = factor(senator, levels = c("SenBobCasey", "SenToomey"))) %>%
   unnest_tokens(word, text, token = "regex", pattern = unnest_reg) %>%
   filter(!word %in% stop_words$word,
          !word %in% c("009f", "00a6", "f0"),
@@ -59,15 +63,19 @@ frequency
 frequency <- frequency %>% 
   select(senator, word, freq) %>% 
   spread(senator, freq) %>%
-  arrange(desc(Casey), desc(Toomey))
+  arrange(desc(SenBobCasey), desc(SenToomey))
 frequency
 
-ggplot(frequency, aes(Casey, Toomey)) +
+ggplot(frequency, aes(SenBobCasey, SenToomey)) +
   geom_jitter(alpha = 0.1, size = 2.5, width = 0.25, height = 0.25) +
   geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
   scale_x_log10(labels = percent_format()) +
   scale_y_log10(labels = percent_format()) +
-  geom_abline(color = "red")
+  geom_abline(color = "red") +
+  labs(title = title,
+       x = "Used more by @SenBobCasey",
+       y = "Used more by @SenToomey",
+       caption = caption)
 
 word_ratios <- tidy_tweets %>%
   filter(!str_detect(word, "^@")) %>%
@@ -76,7 +84,7 @@ word_ratios <- tidy_tweets %>%
   ungroup() %>%
   spread(senator, n, fill = 0) %>%
   mutate_if(is.numeric, funs((. + 1) / sum(. + 1))) %>%
-  mutate(logratio = log(Casey / Toomey)) %>%
+  mutate(logratio = log(SenBobCasey / SenToomey)) %>%
   arrange(desc(logratio))
 
 word_ratios %>% 
@@ -91,9 +99,10 @@ word_ratios %>%
   ggplot(aes(word, logratio, fill = logratio < 0)) +
   geom_col(alpha = .8) +
   coord_flip() +
-  ylab("log odds ratio (Casey/Toomey)") +
+  labs(x = "",
+       y = "Log odds ratio (Casey/Toomey)") +
   scale_fill_manual(name = "", 
                     values = c("blue", "red"),
                     breaks = c(FALSE, TRUE), 
-                    labels = c("Casey", "Toomey")) +
+                    labels = c("@SenBobCasey", "@SenToomey")) +
   theme(panel.grid.major.y = element_blank())
