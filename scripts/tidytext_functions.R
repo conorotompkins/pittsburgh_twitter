4789ea14e59b6dafe997b6701ccfd4c3c779b641
Conor Tompkins <conorotompkins@gmail.com>
2018-03-09 08:57:40 -0500

    updated functions

--- scripts/tidytext_functions.R
+++ scripts/tidytext_functions.R
@@ -57,22 +57,22 @@ word_correlations <- function(dataframe, minimum, custom_stopwords){
   replace_reg <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
   unnest_reg <- "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
   dataframe %>% 
-  filter(is_quote == FALSE, is_retweet == FALSE) %>% 
-  select(status_id, text) %>% 
-  mutate(section = row_number() %/% 10) %>%
-  filter(section > 0) %>%
-  mutate(text = str_replace_all(text, replace_reg, "")) %>%
-  unnest_tokens(word, text, token = "regex", pattern = unnest_reg) %>%
-  filter(!word %in% stop_words$word,
-         !word %in% custom_stopwords,
-         str_detect(word, "[a-z]")) %>% 
-  mutate(word = str_replace(word, "'", ""),
-         word = str_replace(word, "’", ""),
-         word = SnowballC::wordStem(word)) %>% 
-  group_by(word) %>% 
-  filter(n() >= minimum) %>%
-  pairwise_cor(word, section, sort = TRUE)
-  }
+    filter(is_quote == FALSE, is_retweet == FALSE) %>% 
+    select(status_id, text) %>% 
+    mutate(section = row_number() %/% 10) %>%
+    filter(section > 0) %>%
+    mutate(text = str_replace_all(text, replace_reg, "")) %>%
+    unnest_tokens(word, text, token = "regex", pattern = unnest_reg) %>%
+    filter(!word %in% stop_words$word,
+           !word %in% custom_stopwords,
+           str_detect(word, "[a-z]")) %>% 
+    mutate(word = str_replace(word, "'", ""),
+           word = str_replace(word, "'", ""),
+           word = SnowballC::wordStem(word)) %>% 
+    group_by(word) %>% 
+    filter(n() >= minimum) %>%
+    pairwise_cor(word, section, sort = TRUE)
+}
 #replace_reg <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
 #unnest_reg <- "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
 #tidy_tweets <- tweets %>% 
@@ -85,15 +85,15 @@ word_correlations <- function(dataframe, minimum, custom_stopwords){
 
 visualize_word_correlations <- function(dataframe, title, subtitle, caption){
   dataframe %>% 
-  filter(correlation > .05) %>%
-  graph_from_data_frame() %>%
-  ggraph(layout = "fr") +
-  geom_edge_link(aes(edge_alpha = correlation, edge_width = correlation), show.legend = FALSE) +
-  geom_node_point(color = "lightblue", size = 5) +
-  geom_node_label(aes(label = name), size = 5, repel = FALSE) +
-  scale_edge_alpha_continuous(range = c(.1, .5)) +
-  theme_void(base_size = 18) +
-  labs(title = title,
-       subtitle = subtitle,
-       caption = caption)
+    filter(correlation > .3) %>%
+    graph_from_data_frame() %>%
+    ggraph(layout = "fr") +
+    geom_edge_link(aes(edge_alpha = correlation, edge_width = correlation), show.legend = FALSE) +
+    geom_node_point(color = "lightblue", size = 5) +
+    geom_node_label(aes(label = name), size = 5, repel = TRUE) +
+    scale_edge_alpha_continuous(range = c(.1, .5)) +
+    theme_void(base_size = 18) +
+    labs(title = title,
+         subtitle = subtitle,
+         caption = caption)
 }
